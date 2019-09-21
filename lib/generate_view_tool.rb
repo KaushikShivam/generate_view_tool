bf4deaf72ac253903b478503fb35513fd87d3190
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-21 16:49:20 +0530

    Initial commit

---
+++ lib/generate_view_tool.rb
@@ -0,0 +1,7 @@
+require "generate_view_tool/version"
+require "generate_view_tool/renderer"
+
+module GenerateViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/generate_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GenerateViewTool
+  VERSION = "0.1.0"
+end
